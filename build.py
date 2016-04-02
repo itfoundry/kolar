8844f25c942acc31ed5ced26201d2a742cacbfa4
Liang Hai <lianghai@gmail.com>
2016-04-02 23:44:54 +0800

    Update build system

--- build.py
+++ build.py
@@ -3,48 +3,46 @@
 from __future__ import division, absolute_import, print_function, unicode_literals
 import hindkit as kit
 
+def generate(self):
+    self.import_glyphs_from(
+        source_dir = 'masters/Latin/',
+        target_dir = 'masters/Kannada/',
+        excluding_names = 'space NULL CR'.split(),
+    )
+    self.derive_glyphs('NULL CR'.split())
+
+kit.Master.generate = generate
+
+# ---
+
 family = kit.Family(
     client = 'Google Fonts',
     trademark = 'Kolar',
     script = 'Kannada',
 )
+
 family.info.openTypeNameDesigner = "Ramakrishna Saiteja (Kannada); Shiva Nallaperumal (Latin)"
+family.info.openTypeHheaAscender, family.info.openTypeHheaDescender = (1050, -450)
 
 family.set_masters()
-family.set_styles(
-    style_scheme = [
-        ('Light',       0, 300),
-        ('Regular',     9, 400),
-        ('Medium',     26, 500),
-        ('SemiBold',   49, 600),
-        ('Bold',       76, 700),
-        ('ExtraBold', 100, 800),
-    ],
-)
+family.set_styles([
+    ('Light',       0, 300),
+    ('Regular',     9, 400),
+    ('Medium',     26, 500),
+    ('SemiBold',   49, 600),
+    ('Bold',       76, 700),
+    ('ExtraBold', 100, 800),
+])
 
-def prepare_master(self, master):
-    master.import_glyphs_from(
-        source_dir = 'masters/Latin/',
-        target_dir = 'masters/Kannada/',
-        excluding_names = 'space NULL CR'.split(),
-    )
-    master.derive_glyphs('NULL CR'.split())
+# ---
 
-kit.Builder.prepare_master = prepare_master
-
-builder = kit.Builder(
+project = kit.Project(
     family,
     fontrevision = '1.000',
-    vertical_metrics = {
-        'Ascender': 1050,
-        'Descender': -450,
-        'TypoAscender': 800,
-        'TypoDescender': -200,
-    },
     options = {
         # 'prep_mark_positioning': True,
-        'override_GDEF': True,
         'do_style_linking': True,
+        'build_ttf': True,
     },
 )
-builder.build()
+project.build()
